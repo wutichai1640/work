ad2b7b95b37673ffe220713bc4f661a979725c46
Wutichai Hutaman <wutichai_1640@hotmail.com>
2022-07-06 11:17:37 +0700

    first commit

---
+++ fruits.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+}
+
+let apple = 3;
+let banana = 2;
+let total = add(apple, banana);
+
+console.log("I have %d apples.", apple);
+console.log("You have %d bananas.", banana);
+console.log("We have %d fruits in total.", total);
